5960f71e88b4ced6be137d07afef34e083d66d20
rebekacep <rebeka.cep@gmail.com>
2026-06-12 01:01:57 +0200

    Test dvc pull in produciton

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Starting Hydro Risk service..."
+echo "Pulling DVC artifacts..."
+
+poetry run dvc pull \
+  validate_data \
+  test_data \
+  prepare_production_data \
+  train_hidro \
+  train_risk_classifier -v
+
+echo "Starting service..."
 
 if [ "$SERVICE_TYPE" = "ui" ]; then
   streamlit run src/ui/app.py --server.port=${PORT:-8501} --server.address=0.0.0.0
